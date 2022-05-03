9837a9485644c850e0e0beab20358317acc52982
barkharani4ever <bbp45@njit.edu>
2022-05-03 19:12:11 -0400

    Project 3 : google_map locations final changes

--- app/map/forms/__init__.py
+++ app/map/forms/__init__.py
@@ -2,6 +2,41 @@ from flask_wtf import FlaskForm
 from wtforms import validators
 from wtforms.fields import *
 
+
+class location_edit_form(FlaskForm):
+    title = TextAreaField('title', [validators.length(min=6, max=300)],
+                          description="Add title")
+    longitude = TextAreaField('longitude', [validators.length(min=6, max=300)],
+                              description="Add longitude")
+    latitude = TextAreaField('latitude', [validators.length(min=6, max=300)],
+                             description="Add latitude")
+    population = IntegerField('population', [validators.DataRequired()], description="Add population")
+
+    submit = SubmitField()
+
+class location_add_form(FlaskForm):
+    title = TextAreaField('Add title', [
+        validators.length(min=6, max=300),
+
+    ], description="Title")
+
+    longitude = TextAreaField('Add longitude', [
+        validators.length(min=6, max=300),
+
+    ], description="Longitude")
+
+    latitude = TextAreaField('Add latitude', [
+        validators.length(min=6, max=300),
+
+    ], description="Latitude")
+
+    population = IntegerField('Add population', [
+        validators.DataRequired(),
+
+    ], description="Population")
+
+    submit = SubmitField()
+
 class csv_upload(FlaskForm):
     file = FileField()
     submit = SubmitField()
